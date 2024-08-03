41690a94f45decb3267f104ee6d7fe960361b6a6
JeanCarlos <jmatochepascual@gmail.com>
2024-08-02 21:27:16 -0500

    [FEATURE] Agregar componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// Importar componentes
+import Header from "./components/Header"
+
 
 function App() {
 
   return (
-    <div>
-
+    <div className="w-11/12 max-w-xl mx-auto p-8 rounded-md shadow-lg bg-white">
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='mb-12 text-4xl text-center font-extrabold text-gray-600 md:text-6xl'>
+      ¿Cuánto <span className='text-indigo-600'>Dinero</span> necesitas?
+    </header>
+  )
+}
+
+export default Header
